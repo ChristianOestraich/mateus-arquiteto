a7f7ad32a21cb035ecac2ce86d54133c8a2f3e37
c1342354 Christian <c1342354@interno.bb.com.br>
2025-11-28 20:13:51 -0300

    ajuste nas paginas

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,ts}", "./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0d0d0d",
+        accent: "#c9a86a",
+        soft: "#f3f3f3",
+      },
+    },
+  },
+  plugins: [],
+};
